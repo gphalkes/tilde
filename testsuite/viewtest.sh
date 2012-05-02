0aca9a73e940496db499336053a95b8e4382a023
Gertjan Halkes <hg@ghalkes.nl>
2012-05-02 21:50:44 +0200

    Make viewtest work

--- testsuite/viewtest.sh
+++ testsuite/viewtest.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-fail() {
-	echo "$@" >&2
-	exit 1
-}
+DIR="`dirname \"$0\"`"
+. "$DIR/_common.sh"
 
 if [ $# -ne 1 ] ; then
-	fail "Usage: runtest.sh <dir with test>"
+	fail "Usage: viewtest.sh <dir with test>"
 fi
 
-[ "${1#/}" = "$1" ] && [ "${1#~}" = "$1" ] && TEST="$PWD/$1"
-
-DIR="`dirname \"$0\"`"
-
-cd "$DIR" || fail "Could not change to base dir"
-{ [ -d work ] || mkdir work ; } || fail "Could not create work dir"
-cd work || fail "Could not change to work dir"
+setup_TEST "$1"
+cd_workdir
 
 rm -rf *
 
 cp -r "$TEST"/* . || fail "Could not copy test"
 cd context || fail "Could not cd into context dir"
-../../../../record/src/view $REPLAYOPTS ../recording || fail "!! Terminal output is different"
+../../../../../record/src/tdview $REPLAYOPTS ../recording || fail "!! Terminal output is different"
