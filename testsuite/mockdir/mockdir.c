47dfbca1b8a96d91e6c4523d2f66783d7f0fb4d4
Gertjan Halkes <git@ghalkes.nl>
2013-03-09 09:03:43 +0100

    Just initialize all mock_dir functions on library init

--- testsuite/mockdir/mockdir.c
+++ testsuite/mockdir/mockdir.c
@@ -28,20 +28,16 @@ static const size_t mock_dir_len = sizeof(mock_dir) - 1;
 static char *base_dir;
 static size_t base_dir_len;
 
-#define DEF_LIBC_FUNC(retval, name, ...) static retval (*real_##name)(__VA_ARGS__)
-#define INIT_LIBC_FUNC(name) do { if (real_##name == NULL) real_##name = get_libc_func(#name); } while (0)
-
-DEF_LIBC_FUNC(int, open, const char *name, int flags);
-DEF_LIBC_FUNC(char *, getcwd, char *buf, size_t size);
-DEF_LIBC_FUNC(char *, realpath, const char *path, char *resolved_path);
-DEF_LIBC_FUNC(int, stat, const char *path, struct stat *buf);
-DEF_LIBC_FUNC(int, rename, const char *oldpath, const char *newpath);
-DEF_LIBC_FUNC(int, link, const char *oldpath, const char *newpath);
-DEF_LIBC_FUNC(int, unlink, const char *pathname);
-DEF_LIBC_FUNC(int, mkstemp, char *template);
-DEF_LIBC_FUNC(int, __xstat, int ver, const char *path, struct stat *buf);
-DEF_LIBC_FUNC(int, creat, const char *name, mode_t mode);
-
+static int (*real_open)(const char *name, int flags);
+static char *(*real_getcwd)(char *buf, size_t size);
+static char *(*real_realpath)(const char *path, char *resolved_path);
+static int (*real_stat)(const char *path, struct stat *buf);
+static int (*real_rename)(const char *oldpath, const char *newpath);
+static int (*real_link)(const char *oldpath, const char *newpath);
+static int (*real_unlink)(const char *pathname);
+static int (*real_mkstemp)(char *template);
+static int (*real___xstat)(int ver, const char *path, struct stat *buf);
+static int (*real_creat)(const char *name, mode_t mode);
 
 #define FATAL(...) fatal(__LINE__, __VA_ARGS__)
 #ifdef __GNUC__
@@ -49,6 +45,8 @@ void fatal(int line, const char *fmt, ...) __attribute__((format (printf, 2, 3))
 #else
 /*@noreturn@*/ void fatal(int line, const char *fmt, ...);
 #endif
+
+/*=============== Utility functions ===============*/
 
 /** Alert the user of a fatal error and quit.
     @param fmt The format string for the message. See fprintf(3) for details.
@@ -94,11 +92,9 @@ static void *get_libc_func(const char *name) {
 static void init_base_dir(void) {
 	if (base_dir == NULL) {
 		if ((base_dir = getenv("MOCK_DIR")) == NULL) {
-			INIT_LIBC_FUNC(getcwd);
 			if ((base_dir = real_getcwd(NULL, 0)) == NULL)
 				FATAL("getcwd failed: %m\n");
 		} else {
-			INIT_LIBC_FUNC(realpath);
 			base_dir = real_realpath(base_dir, NULL);
 		}
 		base_dir_len = strlen(base_dir);
@@ -109,7 +105,6 @@ static char *get_real_name(const char *name) {
 	char *name_buffer;
 
 	if (strncmp(name, mock_dir, mock_dir_len) == 0 && name[mock_dir_len] == '/') {
-		init_base_dir();
 		name_buffer = strcat_auto(base_dir, name + mock_dir_len);
 		return name_buffer;
 	} else {
@@ -120,8 +115,6 @@ static char *get_real_name(const char *name) {
 static char *get_mock_name(const char *name) {
 	char *result;
 	size_t name_len;
-
-	init_base_dir();
 
 	name_len = strlen(name);
 
@@ -134,10 +127,10 @@ static char *get_mock_name(const char *name) {
 	return result;
 }
 
+/*=============== Mocking functions that replace the real functions from libc. ===============*/
+
 char *getcwd(char *buf, size_t size) {
 	char *result, *mock_name;
-
-	INIT_LIBC_FUNC(getcwd);
 
 	result = real_getcwd(NULL, 0);
 	mock_name = get_mock_name(result);
@@ -159,8 +152,6 @@ int open(const char *name, int flags) {
 	char *real_name;
 	int result;
 
-	INIT_LIBC_FUNC(open);
-
 	real_name = get_real_name(name);
 	result = real_open(real_name, flags);
 	free(real_name);
@@ -170,9 +161,6 @@ int open(const char *name, int flags) {
 char *realpath(const char *path, char *resolved_path) {
 	char *real_name, *mock_name;
 	char *result;
-
-	INIT_LIBC_FUNC(realpath);
-	INIT_LIBC_FUNC(getcwd);
 
 	real_name = get_real_name(path);
 	result = real_realpath(real_name, resolved_path);
@@ -202,8 +190,6 @@ int stat(const char *path, struct stat *buf) {
 	char *real_name;
 	int result;
 
-	INIT_LIBC_FUNC(stat);
-
 	real_name = get_real_name(path);
 	result = real_stat(real_name, buf);
 	free(real_name);
@@ -214,8 +200,6 @@ int __xstat(int ver, const char *path, struct stat *buf) {
 	char *real_name;
 	int result;
 
-	INIT_LIBC_FUNC(__xstat);
-
 	real_name = get_real_name(path);
 	result = real___xstat(ver, real_name, buf);
 	free(real_name);
@@ -225,8 +209,6 @@ int __xstat(int ver, const char *path, struct stat *buf) {
 int rename(const char *oldpath, const char *newpath) {
 	char *real_oldname, *real_newname;
 	int result;
-
-	INIT_LIBC_FUNC(rename);
 
 	real_oldname = get_real_name(oldpath);
 	real_newname = get_real_name(newpath);
@@ -240,8 +222,6 @@ int link(const char *oldpath, const char *newpath) {
 	char *real_oldname, *real_newname;
 	int result;
 
-	INIT_LIBC_FUNC(link);
-
 	real_oldname = get_real_name(oldpath);
 	real_newname = get_real_name(newpath);
 	result = real_link(real_oldname, real_newname);
@@ -254,8 +234,6 @@ int unlink(const char *pathname) {
 	char *real_name;
 	int result;
 
-	INIT_LIBC_FUNC(unlink);
-
 	real_name = get_real_name(pathname);
 	result = real_unlink(real_name);
 	free(real_name);
@@ -267,8 +245,6 @@ int mkstemp(char *template) {
 	int result;
 	size_t template_len;
 	size_t real_name_len;
-
-	INIT_LIBC_FUNC(mkstemp);
 
 	real_name = get_real_name(template);
 	result = real_mkstemp(real_name);
@@ -284,14 +260,16 @@ int creat(const char *name, mode_t mode) {
 	char *real_name;
 	int result;
 
-	INIT_LIBC_FUNC(creat);
-
 	real_name = get_real_name(name);
 	result = real_creat(real_name, mode);
 	free(real_name);
 	return result;
 }
 
+/*=============== library initialization and clean up ===============*/
+/* For gcc (and compatible compilers) we use the __attribute__ method of
+   defining constructor and destructor. For all others, we hope that the _init
+   _fini method still work. */
 #ifdef __GNUC__
 void __attribute__((constructor)) lib_init(void);
 void __attribute__((destructor)) lib_fini(void);
@@ -300,7 +278,29 @@ void __attribute__((destructor)) lib_fini(void);
 #define lib_fini _fini
 #endif
 
+
+#define INIT_LIBC_FUNC(name, may_fail) init_libc_func(#name, (void **) &real_##name, may_fail)
+static void init_libc_func(const char *name, void **func_ptr, int may_fail) {
+	*func_ptr = get_libc_func(name);
+	if (!may_fail && *func_ptr == NULL)
+		FATAL("failed to get handle for library function %s\n", name);
+}
+
 void lib_init(void) {
+	INIT_LIBC_FUNC(open, 0);
+	INIT_LIBC_FUNC(getcwd, 0);
+	INIT_LIBC_FUNC(realpath, 0);
+	INIT_LIBC_FUNC(rename, 0);
+	INIT_LIBC_FUNC(link, 0);
+	INIT_LIBC_FUNC(unlink, 0);
+	INIT_LIBC_FUNC(mkstemp, 0);
+	INIT_LIBC_FUNC(creat, 0);
+
+	INIT_LIBC_FUNC(stat, 1);
+	INIT_LIBC_FUNC(__xstat, 1);
+	if (real_stat == NULL && real___xstat == NULL)
+		FATAL("failed to get a handle for stat/__xstat\n");
+
 	init_base_dir();
 }
 
