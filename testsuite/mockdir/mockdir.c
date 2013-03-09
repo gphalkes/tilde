e7f902a8aef0d8022897ae084e7d53ac11bac385
Gertjan Halkes <git@ghalkes.nl>
2013-03-09 08:32:50 +0100

    Make mock dir configurable at runtime Add start-up and clean-up code to mockdir

--- testsuite/mockdir/mockdir.c
+++ testsuite/mockdir/mockdir.c
@@ -9,6 +9,11 @@
 #include <sys/stat.h>
 
 static const char mock_dir[] = "/mockdir";
+static const size_t mock_dir_len = sizeof(mock_dir) - 1;
+
+static char *base_dir;
+static size_t base_dir_len;
+
 #define DEF_LIBC_FUNC(retval, name, ...) static retval (*real_##name)(__VA_ARGS__)
 #define INIT_LIBC_FUNC(name) do { if (real_##name == NULL) real_##name = get_libc_func(#name); } while (0)
 
@@ -23,51 +28,115 @@ DEF_LIBC_FUNC(int, mkstemp, char *template);
 DEF_LIBC_FUNC(int, __xstat, int ver, const char *path, struct stat *buf);
 DEF_LIBC_FUNC(int, creat, const char *name, mode_t mode);
 
+
+#define FATAL(...) fatal(__LINE__, __VA_ARGS__)
+#ifdef __GNUC__
+void fatal(int line, const char *fmt, ...) __attribute__((format (printf, 2, 3))) __attribute__((noreturn));
+#else
+/*@noreturn@*/ void fatal(int line, const char *fmt, ...);
+#endif
+
 /** Alert the user of a fatal error and quit.
     @param fmt The format string for the message. See fprintf(3) for details.
     @param ... The arguments for printing.
 */
-void fatal(const char *fmt, ...) {
+void fatal(int line, const char *fmt, ...) {
 	va_list args;
 
+	fprintf(stderr, "mockdir:%d: ", line);
 	va_start(args, fmt);
 	vfprintf(stderr, fmt, args);
 	va_end(args);
-	exit(126);
+	abort();
 }
 
+static char *strcat_auto(const char *a, const char *b) {
+	char *result;
+
+	if ((result = malloc(strlen(a) + strlen(b) + 1)) == NULL)
+		FATAL("out of memory\n");
+
+	return strcat(strcpy(result, a), b);
+}
+
+static char *safe_strdup(const char *str) {
+	char *result = strdup(str);
+	if (result == NULL)
+		FATAL("out of memory\n");
+	return result;
+}
 
 static void *get_libc_func(const char *name) {
 	static void *libc;
 
 	if (libc == NULL) {
 		if ((libc = dlopen("libc.so.6", RTLD_LAZY)) == NULL)
-			exit(127);
+			FATAL("dlopen failed: %s\n", dlerror());
 	}
 
 	return dlsym(libc, name);
 }
 
-static char *get_real_name(const char *name) {
-	INIT_LIBC_FUNC(getcwd);
-
-	if (strncmp(name, mock_dir, sizeof(mock_dir) - 1) == 0 && name[sizeof(mock_dir) - 1] == '/') {
-		char name_buffer[PATH_MAX];
-		if (real_getcwd(name_buffer, sizeof(name_buffer)) == NULL)
-			fatal("mockdir:%d: getcwd failed: %m\n", __LINE__);
-		strcat(name_buffer, name + sizeof(mock_dir) - 1);
-		return strdup(name_buffer);
-	} else {
-		return strdup(name);
+static void init_base_dir(void) {
+	if (base_dir == NULL) {
+		if ((base_dir = getenv("MOCK_DIR")) == NULL) {
+			INIT_LIBC_FUNC(getcwd);
+			if ((base_dir = real_getcwd(NULL, 0)) == NULL)
+				FATAL("getcwd failed: %m\n");
+		} else {
+			base_dir = safe_strdup(base_dir);
+		}
+		base_dir_len = strlen(base_dir);
 	}
 }
 
+static char *get_real_name(const char *name) {
+	char *name_buffer;
+
+	if (strncmp(name, mock_dir, mock_dir_len) == 0 && name[mock_dir_len] == '/') {
+		init_base_dir();
+		name_buffer = strcat_auto(base_dir, name + mock_dir_len);
+		return name_buffer;
+	} else {
+		return safe_strdup(name);
+	}
+}
+
+static char *get_mock_name(const char *name) {
+	char *result;
+	size_t name_len;
+
+	init_base_dir();
+
+	name_len = strlen(name);
+
+	if (base_dir_len > name_len)
+		result = safe_strdup(name);
+	else if (strncmp(name, base_dir, base_dir_len) == 0 && (name[base_dir_len] == '/' || base_dir_len == name_len))
+		result = strcat_auto(mock_dir, name + base_dir_len);
+	else
+		result = safe_strdup(name);
+	return result;
+}
+
 char *getcwd(char *buf, size_t size) {
-	if (size < sizeof(mock_dir)) {
+	char *result, *mock_name;
+
+	INIT_LIBC_FUNC(getcwd);
+
+	result = real_getcwd(NULL, 0);
+	mock_name = get_mock_name(result);
+	free(result);
+
+	if (buf == NULL)
+		return mock_name;
+
+	if (size < strlen(mock_name) + 1) {
 		errno = ERANGE;
 		return NULL;
 	}
-	strcpy(buf, mock_dir);
+	strcpy(buf, mock_name);
+	free(mock_name);
 	return buf;
 }
 
@@ -84,17 +153,11 @@ int open(const char *name, int flags) {
 }
 
 char *realpath(const char *path, char *resolved_path) {
-	char cwd_buffer[PATH_MAX];
-	char *real_name;
+	char *real_name, *mock_name;
 	char *result;
-	size_t cwd_buffer_len;
 
 	INIT_LIBC_FUNC(realpath);
 	INIT_LIBC_FUNC(getcwd);
-
-	if (real_getcwd(cwd_buffer, sizeof(cwd_buffer)) == NULL)
-		fatal("mockdir:%d: getcwd failed: %m\n", __LINE__);
-	cwd_buffer_len = strlen(cwd_buffer);
 
 	real_name = get_real_name(path);
 	result = real_realpath(real_name, resolved_path);
@@ -103,14 +166,21 @@ char *realpath(const char *path, char *resolved_path) {
 	if (result == NULL)
 		return NULL;
 
-	if (strncmp(result, cwd_buffer, cwd_buffer_len) == 0 && result[cwd_buffer_len] == '/') {
-		if (cwd_buffer_len < sizeof(mock_dir) - 1)
-			fatal("mockdir:%d: current working directory name shorter than %s\n", __LINE__, mock_dir);
-		memcpy(result, mock_dir, sizeof(mock_dir) - 1);
-		memmove(result + sizeof(mock_dir) - 1, result + cwd_buffer_len, strlen(result) - cwd_buffer_len + 1);
+	mock_name = get_mock_name(result);
+	if (resolved_path == NULL) {
+		free(result);
+		return mock_name;
+	} else {
+		if (strlen(mock_name) + 1 < PATH_MAX) {
+			strcpy(resolved_path, mock_name);
+			free(mock_name);
+			return resolved_path;
+		} else {
+			free(mock_name);
+			errno = ENAMETOOLONG;
+			return NULL;
+		}
 	}
-
-	return result;
 }
 
 int stat(const char *path, struct stat *buf) {
@@ -205,4 +275,20 @@ int creat(const char *name, mode_t mode) {
 	result = real_creat(real_name, mode);
 	free(real_name);
 	return result;
+}
+
+#ifdef __GNUC__
+void __attribute__((constructor)) lib_init(void);
+void __attribute__((destructor)) lib_fini(void);
+#else
+#define lib_init _init
+#define lib_fini _fini
+#endif
+
+void lib_init(void) {
+	init_base_dir();
+}
+
+void lib_fini(void) {
+	free(base_dir);
 }
