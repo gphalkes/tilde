f676e1005ae2ed560894a6f9177561f4f62782e9
Gertjan Halkes <git@ghalkes.nl>
2018-09-09 13:01:54 +0200

    Revert "Add copy_file routine (not fully done), with unittest"
    This reverts commit e123d6250c59485afac71c205f716dec44dfea8d.
    
    This commit is part of in-progress work that is not fully functinoal yet.

--- unittests/cleanup_copy_file_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-udisksctl unmount --block-device ${EXT3_LOOP}
-udisksctl unmount --block-device ${BTRFS_LOOP}
-udisksctl loop-delete --block-device ${EXT3_LOOP}
-udisksctl loop-delete --block-device ${BTRFS_LOOP}
