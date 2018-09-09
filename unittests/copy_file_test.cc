2b0e0b345bce020e937acdbcc8e93a6e24d3cc80
Gertjan Halkes <git@ghalkes.nl>
2018-09-09 13:00:41 +0200

    Revert "cleanups for copy_file and configure tests for new functionality"
    This reverts commit 4aa1499e5299e099c150b0965eeba7f2d1a8fc32.
    
    This commit is part of in-progress work that is not fully functinoal yet.

--- unittests/copy_file_test.cc
+++ unittests/copy_file_test.cc
@@ -15,41 +15,35 @@ namespace {
 class QcheckIo {
  public:
   virtual ~QcheckIo() {}
-  virtual QcheckIo &operator<<(const std::string &str) = 0;
-  virtual QcheckIo &operator<<(ssize_t i) = 0;
-  virtual QcheckIo &operator<<(size_t i) = 0;
+  virtual QcheckIo& operator<<(const std::string &str) = 0;
+  virtual QcheckIo& operator<<(ssize_t i) = 0;
+  virtual QcheckIo& operator<<(size_t i) = 0;
 };
 
 class NoOpQcheckIo : public QcheckIo {
  public:
-  QcheckIo &operator<<(const std::string &) override { return *this; }
-  QcheckIo &operator<<(ssize_t) override { return *this; }
-  QcheckIo &operator<<(size_t) override { return *this; }
+  QcheckIo& operator<<(const std::string &) override { return *this; }
+  QcheckIo& operator<<(ssize_t) override { return *this; }
+  QcheckIo& operator<<(size_t) override { return *this; }
 };
 
 class FatalQcheckIo : public QcheckIo {
  public:
-  FatalQcheckIo(const char *condition) { std::cerr << "Check failed: " << condition; }
+  FatalQcheckIo(const char *condition) {
+    std::cerr << "Check failed: " << condition;
+  }
   ~FatalQcheckIo() {
     std::cerr << "\n";
     std::cerr.flush();
     abort();
   }
-  QcheckIo &operator<<(const std::string &str) override {
+  QcheckIo& operator<<(const std::string &str) override {
     NextOutput();
     std::cerr << str;
     return *this;
   }
-  QcheckIo &operator<<(ssize_t i) override {
-    NextOutput();
-    std::cerr << i;
-    return *this;
-  }
-  QcheckIo &operator<<(size_t i) override {
-    NextOutput();
-    std::cerr << i;
-    return *this;
-  }
+  QcheckIo& operator<<(ssize_t i) override { NextOutput(); std::cerr << i; return *this; }
+  QcheckIo& operator<<(size_t i) override { NextOutput(); std::cerr << i; return *this; }
 
  private:
   void NextOutput() {
@@ -65,19 +59,12 @@ class FatalQcheckIo : public QcheckIo {
 class QcheckIoWrapper : public QcheckIo {
  public:
   QcheckIoWrapper(std::unique_ptr<QcheckIo> wrapped) : wrapped_(std::move(wrapped)) {}
-  QcheckIo &operator<<(const std::string &str) override {
+  QcheckIo& operator<<(const std::string &str) override {
     (*wrapped_) << str;
     return *this;
   }
-  QcheckIo &operator<<(ssize_t i) override {
-    (*wrapped_) << i;
-    return *this;
-  }
-  QcheckIo &operator<<(size_t i) override {
-    (*wrapped_) << i;
-    return *this;
-  }
-
+  QcheckIo& operator<<(ssize_t i) override { (*wrapped_) << i; return *this; }
+  QcheckIo& operator<<(size_t i) override { (*wrapped_) << i; return *this; }
  private:
   std::unique_ptr<QcheckIo> wrapped_;
 };
@@ -106,17 +93,14 @@ bool FileCopied(const std::string &a, const std::string &b) {
     ssize_t read_b = t3_widget::nosig_read(fd_b, buffer_b, sizeof(buffer_b));
 
     if (read_a != read_b) {
-      std::cerr << "File " << a << " and file " << b
-                << " have different lengths. Read results at offset " << offset << ": a=" << read_a
-                << " b=" << read_b << "\n";
+      std::cerr << "File " << a << " and file " << b << " have different lengths. Read results at offset " << offset << ": a=" << read_a << " b=" << read_b << "\n";
       return false;
     }
     if (read_a == 0) {
       return true;
     }
     if (std::memcmp(buffer_a, buffer_b, read_a) != 0) {
-      std::cerr << "File " << a << " and file " << b
-                << " have different contents when reading at offset " << offset << "\n";
+      std::cerr << "File " << a << " and file " << b << " have different contents when reading at offset " << offset << "\n";
       return false;
     }
     offset += read_a;
@@ -140,10 +124,8 @@ std::pair<std::string, int> CreateFile(const std::string &dir) {
 std::pair<std::string, int> CreateFileWithContent(std::string content, const std::string &dir) {
   auto name_and_fd = CreateFile(dir);
 
-  QCHECK(t3_widget::nosig_write(name_and_fd.second, content.data(), content.size()) ==
-         static_cast<ssize_t>(content.size()))
-      << "Failed to write full content to file (" << strerror(errno) << ", " << name_and_fd.first
-      << ")";
+  QCHECK(t3_widget::nosig_write(name_and_fd.second, content.data(), content.size()) == static_cast<ssize_t>(content.size())) <<
+      "Failed to write full content to file (" << strerror(errno) << ", " << name_and_fd.first << ")";
   QCHECK(fsync(name_and_fd.second) == 0);
   return name_and_fd;
 }
@@ -155,10 +137,8 @@ void FillWithRandomData(const std::pair<std::string, int> &name_and_fd, size_t s
       buffer[i] = std::rand();
     }
     size_t to_write = std::min(size, sizeof(buffer));
-    QCHECK(t3_widget::nosig_write(name_and_fd.second, buffer, to_write) ==
-           static_cast<ssize_t>(to_write))
-        << "Failed to write full content to file (" << strerror(errno) << ", " << name_and_fd.first
-        << ")";
+    QCHECK(t3_widget::nosig_write(name_and_fd.second, buffer, to_write) == static_cast<ssize_t>(to_write)) <<
+        "Failed to write full content to file (" << strerror(errno) << ", " << name_and_fd.first << ")";
     size -= to_write;
   }
 }
@@ -263,8 +243,7 @@ TEST_F(CopyFileTest, CopyFileRangeWithContentCrossFs) {
   src_name_and_fd_ = CreateFileWithContent("abcd", FLAGS_reflink_fs_dir);
   dest_name_and_fd_ = CreateFile(FLAGS_non_reflink_fs_dir);
 
-  EXPECT_EQ(copy_file_by_copy_file_range(src_name_and_fd_.second, dest_name_and_fd_.second, 4),
-            EXDEV);
+  EXPECT_EQ(copy_file_by_copy_file_range(src_name_and_fd_.second, dest_name_and_fd_.second, 4), EXDEV);
 }
 
 TEST_F(CopyFileTest, CopyFileRangeWithContentReflinkFs) {
@@ -298,7 +277,7 @@ TEST_F(CopyFileTest, FicloneWithContentNonReflinkFs) {
   EXPECT_EQ(copy_file_by_ficlone(src_name_and_fd_.second, dest_name_and_fd_.second), ENOTSUP);
 }
 
-}  // namespace
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
@@ -313,14 +292,13 @@ int main(int argc, char **argv) {
   }
 
   QCHECK(!FLAGS_reflink_fs_dir.empty()) << "--reflink_fs_dir or REFLINK_MOUNT must be set";
-  QCHECK(!FLAGS_non_reflink_fs_dir.empty())
-      << "--non_reflink_fs_dir or NONREFLINK_MOUNT must be set";
+  QCHECK(!FLAGS_non_reflink_fs_dir.empty()) << "--non_reflink_fs_dir or NONREFLINK_MOUNT must be set";
 
   struct stat reflink_stat;
   struct stat non_reflink_stat;
   QCHECK(stat(FLAGS_reflink_fs_dir.c_str(), &reflink_stat) == 0);
   QCHECK(stat(FLAGS_non_reflink_fs_dir.c_str(), &non_reflink_stat) == 0);
-  QCHECK(reflink_stat.st_dev != non_reflink_stat.st_dev)
-      << "reflink enabled and non-reflink enabled file systems must be different";
+  QCHECK(reflink_stat.st_dev != non_reflink_stat.st_dev) << "reflink enabled and non-reflink enabled file systems must be different";
   return RUN_ALL_TESTS();
 }
+
